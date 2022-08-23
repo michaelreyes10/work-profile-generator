13c3b0050eaa39ed702164366d4823af4b8eb1e1
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-08-23 13:20:40 -0400

    create employee and manager questions

---
+++ index.js
@@ -0,0 +1,160 @@
+const inquirer = require('inquirer');
+// const genPage = require('./src/page-temp');
+// const { writeFile, copyFile } = require('./dist/gen-site.js');
+
+const getManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInfo => {
+                if (nameInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'Enter your employee ID (Required)',
+            validate: employeeId => {
+                if (employeeId) {
+                    return true;
+                } else {
+                    console.log('Please enter your employee ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter your office number (Required)',
+            validate: officeNum => {
+                if (officeNum) {
+                    return true;
+                } else {
+                    console.log('Please enter your office number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Please select which option you would like to continue with:',
+            choices: ['add an engineer', 'add an intern', 'finish building my team']
+        }
+    ]);
+};
+
+const getEmployee = employeeInfo => {
+    console.log(`
+    ===============
+    Add a New Employee
+    ===============
+    `);
+
+    // If there's no 'employees' array property, create one
+    if (!employeeInfo.employees) {
+        employeeInfo.employees = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the name of engineer? (Required)',
+            validate: engineerName => {
+                if (engineerName) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of engineer!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'Enter your employee ID (Required)',
+            validate: employeeId => {
+                if (employeeId) {
+                    return true;
+                } else {
+                    console.log('Please enter your employee ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Enter your Github username. (Required)',
+            validate: githubUser => {
+                if (githubUser) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+            }
+        }
+    ]).then(employeeInfo => {
+        employeeInfo.employees.push(employeeInfo);
+        if (employeeInfo.confirmAddEmployee) {
+            return promptProject(employeeInfo);
+        } else {
+            return employeeInfo;
+        }
+    });
+};
+
+getManager()
+    .then(getEmployee)
+    .then(employeeInfo => {
+        return genPage(employeeInfo);
+    })
+    .then(pageHTML => {
+        return writeFile(pgHTML);
+    })
+    .then(writeFileResp => {
+        console.log(writeFileResp);
+        return copyFile();
+    })
+    .then(copyFileResp => {
+        console.log(copyFileResp);
+    })
+    .catch(err => {
+        console.log(err);
+    });
