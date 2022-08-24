d26eb06e04fcf16fe8782426601001d899ac44db
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-08-24 11:56:36 -0400

    completed engineer test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Engineers = require('../lib/Engineer');
+const theEngineer = new Engineers('mike', '123456', 'michaelreyesbusiness@gmail.com', 'michaelreyes10');
+
+test('test if we can get the constructor values for the engineer object', () => {
+    expect(theEngineer.name).toBe('mike');
+    expect(theEngineer.id).toBe('123456');
+    expect(theEngineer.email).toBe('michaelreyesbusiness@gmail.com');
+    expect(theEngineer.gitUser).toBe('michaelreyes10');
+});
+
+test('test if we can get the name from the Name() method', () => {
+    expect(theEngineer.Name()).toBe('mike');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(theEngineer.Id()).toBe('123456');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(theEngineer.Email()).toBe('michaelreyesbusiness@gmail.com');
+});
+
+test('test if we can get the github username from the GithubUser() method', () => {
+    expect(theEngineer.GithubUser()).toBe('michaelreyes10');
+}); 
