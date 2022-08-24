fbf806ce725a9f026315acce1b75508d57a89a59
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-08-24 11:17:09 -0400

    completed Managers test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,11 @@
 const Managers = require('../lib/Manager');
-const theManagers = new theManagers('mike', '123456', 'michaelreyesbusiness@gmail.com', '123');
+const theManagers = new Managers ('mike', '123456', 'michaelreyesbusiness@gmail.com', '123');
 
 test('test if we can get the constructor values for the employee object', () => {
     expect(theManagers.name).toBe('mike');
     expect(theManagers.id).toBe('123456');
     expect(theManagers.email).toBe('michaelreyesbusiness@gmail.com');
-    expect(theManagers.OfficeNumb).toBe('123');
+    expect(theManagers.officeNumb).toBe('123');
 });
 
 test('test if we can get the name from the Name() method', () => {
@@ -21,6 +21,6 @@ test('test if we can get the email from the Email() method', () => {
 });
 
 test('test if we can get the office number from the OfficeNum() method', () => {
-    expect(theManagers.OfficeNumb()).toBe('123');
+    expect(theManagers.OfficeNumber()).toBe('123');
 }); 
 
